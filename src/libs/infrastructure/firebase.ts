989c1074de8f62d473cc2bf82c52c2bdcc3d1b0a
tenmakamatani <tenmakamatani0@gmail.com>
2020-12-19 23:19:45 +0900

    Create: AuthProvider and fix env key

--- src/libs/infrastructure/firebase.ts
+++ src/libs/infrastructure/firebase.ts
@@ -3,14 +3,15 @@ import 'firebase/auth';
 
 if (typeof window !== 'undefined' && !firebase.apps.length) {
   firebase.initializeApp({
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.FIREBASE_AUTH_ADMIN,
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.FIREBASE_APP_ID,
-    measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_ADMIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
   });
+  console.log(process.env);
   firebase.auth().setPersistence(firebase.auth.Auth.Persistence.SESSION);
 }
 
---
+++ src/modules/provider/auth.tsx
@@ -0,0 +1,26 @@
+import React, { useState, useEffect } from 'react';
+import { firebase } from '@libs/infrastructure/firebase';
+import { AuthContext } from '@modules/context/auth';
+
+export const AuthProvider: React.FC = ({ children }) => {
+  const [user, setUser] = useState<firebase.User | null>(null);
+
+  useEffect(() => {
+    return firebase.auth().onIdTokenChanged(async (user) => {
+      if (!user) {
+        setUser(null);
+        // nookies.set(undefined, 'token', '');
+      } else {
+        // const token = await user.getIdToken();
+        setUser(user);
+        // nookies.set(undefined, 'token', token);
+      }
+    });
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ user }}>
+      {children}
+    </AuthContext.Provider>
+  )
+}
