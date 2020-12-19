6bed70eeb688a1e23b9d950fb757e0694958c271
tenmakamatani <46468174+tenmakamatani@users.noreply.github.com>
2020-12-19 19:22:09 +0900

    Initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import Link from 'next/link'
+import { Layout } from '../components/organisms/Layout'
+
+const IndexPage = () => (
+  <Layout>
+    <h1>Hello Next.js 👋</h1>
+    <p>
+      <Link href="/about">
+        <a>About</a>
+      </Link>
+    </p>
+  </Layout>
+)
+
+export default IndexPage
