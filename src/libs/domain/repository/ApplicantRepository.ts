63d10e9242180ebe9ff47ffc20423a55233467a5
tenmakamatani <tenmakamatani0@gmail.com>
2021-01-03 15:01:22 +0900

    Update: add apply method to ApplicantRepository

--- src/libs/domain/repository/ApplicantRepository.ts
+++ src/libs/domain/repository/ApplicantRepository.ts
@@ -3,5 +3,6 @@ import { Applicant, RecruiterId } from "../model";
 export abstract class ApplicantRepository {
   abstract async retrieve(recruiterId: RecruiterId): Promise<Applicant[]>;
   abstract async isApplying(recruiterId: RecruiterId): Promise<boolean>;
+  abstract async apply(recruiterId: RecruiterId): Promise<void>;
   abstract async cancel(recruiterId: RecruiterId): Promise<void>;
 }
